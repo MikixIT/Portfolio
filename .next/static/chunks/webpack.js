f3c896fa39a1d3c04b4bfaa9675017d42176eca5
Michael Torres <michael.46@live.it>
2024-10-23 03:16:24 +0200

    Upload favicon and setting, continue the hero section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ed0a1727480b1c60"; }
+/******/ 		__webpack_require__.h = function() { return "bbb920eea27d22c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
