78cddc05229444b0b3a6b89beffcc82bd6097b87
Michael Torres <michael.46@live.it>
2025-03-25 02:53:13 +0100

    remove gsap markers

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,7 +16,6 @@ function Footer() {
       trigger: document.documentElement,
       start: "bottom bottom",
       end: "bottom bottom",
-      markers: true,
       onEnter: () => {
         gsap.to(footer, {
           y: "0%",
